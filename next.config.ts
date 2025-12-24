e672060852ee76c914915db5aec8e7cf4e4bb2f9
BaseAI Works <baseaiworks@gmail.com>
2025-12-24 02:50:53 +0000

    THIS DOENT FIX IT  TEST IT BEFORE THINK YOU FIXED IT

--- next.config.ts
+++ next.config.ts
@@ -26,13 +26,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
